7998ffda880a4f8967c18c567adcdac60046bde3
Talison <talisondaniel@hotmail.com>
2019-02-20 10:43:06 -0300

    Aprimorando bootstrap

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $( ".scroll" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
+});
